a03bfb79dab0b5b875e8b1da1bdcd1dd1e605ca1
Hugo Rodger-Brown <hugo@rodger-brown.com>
2011-02-17 15:05:32 +0000

    Removed redundant "usings" and rogue reference

--- HipChatClient/HttpUtils.cs
+++ HipChatClient/HttpUtils.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.IO;
 using System.Net;
-using System.IO;
+using System.Text;
 
 namespace HipChat
 {
