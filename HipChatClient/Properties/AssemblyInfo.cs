fd6a1a696abc296a979435778c64a75392868a2d
Hugo Rodger-Brown <hugo@rodger-brown.com>
2011-05-14 13:37:01 +0100

    Updated build number (non-breaking changes) to 1.0.1.0

--- HipChatClient/Properties/AssemblyInfo.cs
+++ HipChatClient/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription(".NET wrapper for HipChat API")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("HRB")]
-[assembly: AssemblyProduct("HipChatServer")]
+[assembly: AssemblyProduct("HipChat API library for .NET")]
 [assembly: AssemblyCopyright("Copyright © Hugo Rodger-Brown 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
